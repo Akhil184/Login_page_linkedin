a7a4d4bba00583a718ea4f43bbe873b37bb0b309
Akhil184 <akhilteli55@gmail.com>
2023-03-16 12:22:27 +0530

    optimize code

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -6,10 +6,10 @@ class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
   @override
-  State<SignUpPage> createState() => _LoginPageState();
+  State<SignUpPage> createState() => _SignUpPageState();
 }
 
-class _LoginPageState extends State<SignUpPage> {
+class _SignUpPageState extends State<SignUpPage> {
   bool passwordVisible=false;
 
   @override
